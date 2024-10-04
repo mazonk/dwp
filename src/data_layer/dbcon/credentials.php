ad97a41f1fe1c857cffa30a225dac396a42d82d1
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-10-04 11:15:51 +0200

    update composer.json

---
+++ src/data_layer/dbcon/credentials.php
@@ -0,0 +1,7 @@
+<?php
+$db_dbname = "mysql:dbname=Cinema";
+$db_host = "host=localhost";
+$db_charset = "charset=utf8";
+define("DSN", "$db_dbname; $db_host; $db_charset");
+define("DB_USER", "andriska");
+define("DB_PASS", "123456");
