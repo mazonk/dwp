d5035f64e8585f7b291af9eca90086800d26ce75
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-10-05 13:55:55 +0200

    ticket business entity updated

--- src/business_layer/entity/Ticket.php
+++ src/business_layer/entity/Ticket.php
@@ -1,13 +1,37 @@
 <?php
 class Ticket {
   private int $ticketId;
+  private Seat $seat;
+  private TicketType $ticketType;
+  private Showing $showing;
+  private Reservation $reservation;
 
-  public function __construct(int $ticketId) {
+  public function __construct(int $ticketId, Seat $seat, TicketType $ticketType, Showing $showing, Reservation $reservation) {
     $this->ticketId = $ticketId;
+    $this->seat = $seat;
+    $this->ticketType = $ticketType;
+    $this->showing = $showing;
+    $this->reservation = $reservation;
   }
 
   public function getTicketId(): int {
     return $this->ticketId;
+  }
+
+  public function getSeat(): Seat {
+    return $this->seat;
+  }
+
+  public function getTicketType(): TicketType {
+    return $this->ticketType;
+  }
+
+  public function getShowing(): Showing {
+    return $this->showing;
+  }
+
+  public function getReservation(): Reservation {
+    return $this->reservation;
   }
 
   public function setTicketId(int $ticketId): void {
