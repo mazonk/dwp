79f0b83d07e1b2994c232c43dfd10c7a143f534e
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-10-05 10:52:11 +0200

    openingHour business entity created

---
+++ src/business_layer/entity/OpeningHour.php
@@ -0,0 +1,67 @@
+<?php
+
+enum Day {
+  case monday;
+  case tuesday;
+  case wednesday;
+  case thursday;
+  case friday;
+  case saturday;
+  case sunday;
+
+}
+
+class OpeningHours {
+  private int $openingHourId;
+  private Day $day;
+  private DateTime $openingTime;
+  private DateTime $closingTime;
+  private bool $isCurrent;
+
+  public function __construct (int $openingHourId, Day $day, DateTime $openingTime, DateTime $closingTime, bool $isCurrent) {
+    $this->$openingHourId = $openingHourId;
+    $this->$day = $day;
+    $this->$openingTime = $openingTime;
+    $this->$closingTime = $closingTime;
+    $this->$isCurrent = $isCurrent;
+  }
+
+  public function getOpeningHourId():int {
+    return $this->getOpeningHourId;
+  }
+
+  public function getDay(): Day {
+    return $this->day;
+  }
+
+  public function getOpeningTime(): DateTime {
+    return $this->openingTime;
+  }
+
+  public function getClosingTime(): DateTime {
+    return $this->closingTime;
+  }
+
+  public function getIsCurrent(): bool {
+    return $this->isCurrent;
+  }
+
+  public function setOpeningHourId(int $openingHourId): void {
+    $this->getOpeningHourId = $openingHourId;
+  }
+
+  public function setDay(Day $day): void {
+    $this->day = $day;
+  }
+
+  public function setOpeningTime(DateTime $openingTime): void {
+    $this->openingTime = $openingTime;
+  }
+
+  public function setClosingTime(DateTime $closingTime): void {
+    $this->closingTime = $closingTime;
+  }
+
+  public function setIsCurrent(bool $isCurrent): void {
+    $this->isCurrent = $isCurrent;
+  }
