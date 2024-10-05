3463b882afb39b23e2e88e6dc7610c1e3da576ec
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-10-05 11:17:23 +0200

    ticketType business entity created

---
+++ src/business_layer/entity/TicketType.php
@@ -0,0 +1,47 @@
+<?php
+class TicketType {
+  private int $ticketTypeId;
+  private string $name;
+  private float $price;
+  private string $description;
+
+  public function __construct(int $ticketTypeId, string $name, float $price, string $description) {
+    $this->ticketTypeId = $ticketTypeId;
+    $this->name = $name;
+    $this->price = $price;
+    $this->description = $description;
+  }
+
+  public function getTicketTypeId(): int {
+    return $this->ticketTypeId;
+  }
+
+  public function getName(): string {
+    return $this->name;
+  }
+
+  public function getPrice(): float {
+    return $this->price;
+  }
+
+  public function getDescription(): string {
+    return $this->description;
+  }
+
+  public function setTicketTypeId(int $ticketTypeId): void {
+    $this->ticketTypeId = $ticketTypeId;
+  }
+
+  public function setName(string $name): void {
+    $this->name = $name;
+  }
+
+  public function setPrice(float $price): void {
+    $this->price = $price;
+  }
+
+  public function setDescription(string $description): void {
+    $this->description = $description;
+  }
+}
+?>
