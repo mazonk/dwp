c802566164b53a1b3cbfaae54d36b20a6c67650e
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-10-05 13:28:34 +0200

    address business entity created

--- src/business_layer/entity/Address.php
+++ src/business_layer/entity/Address.php
@@ -1,6 +1,48 @@
 <?php
 class  Address {
+  private int $addressId;
+  private string $street;
+  private string $streetNr;
+  private PostalCode $postalCode;
 
+  public function __construct(int $addressId, string $street, string $streetNr, PostalCode $postalCode) {
+    $this->addressId = $addressId;
+    $this->street = $street;
+    $this->streetNr = $streetNr;
+    $this->postalCode = $postalCode;
+  }
+
+  public function getAddressId(): int {
+    return $this->addressId;
+  }
+
+  public function getStreet(): string {
+    return $this->street;
+  }
+
+  public function getStreetNr(): string {
+    return $this->streetNr;
+  }
+
+  public function getPostalCode(): PostalCode {
+    return $this->postalCode;
+  }
+
+  public function setAddressId(int $addressId): void {
+    $this->addressId = $addressId;
+  }
+
+  public function setStreet(string $street): void {
+    $this->street = $street;
+  }
+
+  public function setStreetNr(string $streetNr): void {
+    $this->streetNr = $streetNr;
+  }
+
+  public function setPostalCode(PostalCode $postalCode): void {
+    $this->postalCode = $postalCode;
+  }
 }
 
 ?>
