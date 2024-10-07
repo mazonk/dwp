f46ef8b2a3da4768e57c69b504d16be7dbcd9638
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-10-07 16:52:54 +0200

    moved pages to src/presentation_layer

--- src/presentation_layer/pages/index.php
+++ src/presentation_layer/pages/index.php
@@ -2,7 +2,7 @@
     <body>
         <h1>Hello World!! WE ROCK</h1>
         <?php
-        require("../../data_layer/dbcon/dbcon.php");
+        require("../src/data_layer/dbcon/dbcon.php");
         $query = $db->prepare("SELECT * FROM Actor");
         $query->execute();
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
@@ -10,8 +10,6 @@
         foreach ($result as $row) {
             echo "Test actor is called: " . $row['firstName'] . " " . $row['lastName'] . " and plays: " . $row['character'] . "." . "<br>";
         }
-
-        echo(phpversion())
         ?>
     </body>
 </html>
