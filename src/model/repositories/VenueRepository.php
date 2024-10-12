0c26df233a9dd6b35716d0106de5a674385047bf
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-10-12 10:52:44 +0200

    Venue get all venues function

--- src/model/repositories/VenueRepository.php
+++ src/model/repositories/VenueRepository.php
@@ -1,3 +1,21 @@
-<?php
+<?php¨
+include "src/model/entity/Venue.php";
 
-?>
+class VenueRepository {
+  include_once "src/model/database/dbcon/dbcon.php";
+  return $db;
+
+  public function getAllVenues(): array {
+    $db = $this->getdb();
+    $query = $db->prepare("SELECT * FROM Venue");
+    $query->execute();
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    $retArray = [];
+    
+    foreach($result as $row) {
+      $retArray[] = new Venue($row['venueId'], $row['name'], $row['phoneNr'], $row['contactEmail'], $row['address']);
+    }
+    return $retArray;
+  }
+
+}
