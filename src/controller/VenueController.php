a77a330f1733a15323217caef0265fee4cd6c63a
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-10-12 11:16:41 +0200

    Venue controller made

--- src/controller/VenueController.php
+++ src/controller/VenueController.php
@@ -1,1 +1,14 @@
 <?php
+include_one "src/model/repositories/VenueRepository.php";
+
+class VenueController {
+  private VenueRepository $venueRepository;
+
+  public function __construct() {
+    $this->venueRepository = new VenueRepository();
+  }
+
+  public function getAllVenues(): array {
+    return $this->venueRepository->getAllVenues();
+  }
+}
