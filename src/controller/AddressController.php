0b12c3f2a4415bb3240d05f2254e0c4c43b7eb54
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-10-13 09:48:37 +0200

    Address repository made

---
+++ src/controller/AddressController.php
@@ -0,0 +1,14 @@
+<?php
+include_once "src/model/repositories/AddressRepository.php";
+
+class AddressController {
+  private AddressRepository= $addressRepository;
+
+  public function __construct() {
+    $this->addressRepository = new AddressRepository();
+  }
+
+  public function getAllAddresses(): array {
+    return $this->addressRepository->getAllAddresses();
+  }
+}
