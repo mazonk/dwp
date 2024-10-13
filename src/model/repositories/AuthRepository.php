f5e1dacc50985a548bf8f635d7e6a7d556453d41
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-10-13 10:21:59 +0200

    some comments in autrepo

--- src/model/repositories/AuthRepository.php
+++ src/model/repositories/AuthRepository.php
@@ -1,11 +1,21 @@
 <?php
 
 class AuthRepository {
+
+    /**
+     * Gets the PDO database connection singleton
+     * @return PDO The database connection
+     */
     private function getdb(): PDO {
         require_once 'src/model/database/dbcon/DatabaseConnection.php';
         return DatabaseConnection::getInstance(); // singleton
     }
-
+    
+    /**
+     * Creates a new user and inserts it into the database
+     * @param User $user The user to create
+     * @return User|null The created user, or null if something went wrong
+     */
     public function createUser(User $user): ?User {
         $db = $this->getdb();
         $query = $db->prepare("INSERT INTO User (firstName, lastName, DoB, email, passwordHash, roleId) VALUES (:firstName, :lastName, :DoB, :email, :passwordHash, :roleId)");
@@ -31,6 +41,14 @@ class AuthRepository {
         }
     }
 
+    /**
+     * Creates a new user if there is already an existing user with the email
+     * address, but the passwordHash is null (i.e. the user was inserted into the db
+     * during ticket purchase, as an anonyumous user). Updates the existing user with
+     * the given information.
+     * @param User $user The user to create
+     * @return User|null The created user, or null if something went wrong
+     */
     public function createUserToExistingEmail(User $user): ?User {
         $db = $this->getdb();
         $query = $db->prepare("UPDATE User 
@@ -67,7 +85,11 @@ class AuthRepository {
         }
     }
     
-
+    /**
+     * Checks if the given email exists in the database
+     * @param string $email the email to check
+     * @return bool true if the email exists, false otherwise
+     */
     public function emailExists(string $email): bool {
         $db = $this->getdb();
         $query = $db->prepare("SELECT * FROM User WHERE email = :email");
@@ -79,7 +101,12 @@ class AuthRepository {
             return false;
         }
     }
-
+    
+    /**
+     * Checks if the given email exists in the database and the password is not null (meaning there's an account with the given email)
+     * @param string $email the email to check
+     * @return bool true if the email exists and the password is not null, false otherwise
+     */
     public function userExists(string $email): bool {
         $db = $this->getdb();
         $query = $db->prepare("SELECT * FROM User WHERE email = :email AND passwordHash IS NOT NULL");
