c984ffd7e36d3c48556c32f22e7b3a06f50e739a
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-10-13 11:27:45 +0200

    change login form's render method

--- src/view/components/LoginForm.php
+++ src/view/components/LoginForm.php
@@ -1,9 +1,9 @@
 <?php
 class LoginForm {
-    public function render(array $formData = [], array $errors = [], string $message = ''): void {
+    public function render(array $formData = [], array $errors = []): void {
         ?>
         <div class="w-full max-w-xs mx-auto flex flex-col ">
-            <form class="space-y-2" action="/dwp/login/action?action=login" method="post">
+            <form class="space-y-2" action="/dwp/login?action=login" method="post">
                 <div>
                     <label for="email">Email:</label>
                     <input type="text" name="email" id="emailInput" 
@@ -26,12 +26,12 @@ class LoginForm {
                 </div>
 
                 <div class="flex flex-col items-center w-full">
+                <?php if (isset($errors['general'])): ?>
+                        <p class='text-red-500 mt-2'><?= $errors['general'] ?></p>
+                    <?php endif; ?>
                     <input class="text-white font-bold py-2 px-4 rounded text-center w-full mt-4 cursor-pointer" 
                         style="background: #FADF24;" type="submit" name="loginButton" value="Login" />
                     <p class="mt-2 -mb-4 text-gray-500"> Don't have an account yet? <a class="underline text-blue-700" href="/dwp/register">Sign up</></p>
-                    <?php if ($message): ?>
-                        <p class='text-red-500 mt-2'><?= htmlspecialchars($message) ?></p>
-                    <?php endif; ?>
                 </div>
             </form>
         </div>
