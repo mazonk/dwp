7a07e1d4b9264e3d9967a158a9aa786c9e080abd
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-10-13 09:45:04 +0200

    Address repository made

---
+++ src/model/repositories/AddressRepository.php
@@ -0,0 +1,40 @@
+<?php
+include "src/model/entity/Address.php";
+include "src/model/entity/PostalCode.php";
+
+class AddressRepository {
+  private function getdb() {
+    include "src/model/database/dbcon/dbcon.php";
+    return $db;
+  }
+
+  public function getAllAddresses(): {
+    $db = $this->getdb();
+    /* Get postal codes */
+    $postalCodeQuery = $db->prepare("SELECT * FROM PostalCode");
+    $postalCodeQuery->execute();
+    $postalCodeResult = $postalCodeQuery->fetchAll(PDO::FETCH_ASSOC);
+    $postalCodeArray = [];
+
+    /* Get addresses */
+    $addressQuery = $db->prepare("SELECT *  FROM `Address`");
+    $addressQuery->execute();
+    $addressResult = $addressQuery->fetchAll(PDO::FETCH_ASSOC);
+    $addressArray = [];
+
+    foreach($postalCodeResult as $row) {
+      $postalCodeArray[] = new PostalCode($row['postalCode'], $row['city']);
+    }
+
+    foreach($addressResult as $addressRow) {
+      foreach($postalCodeArray as $postalCodeRow) {
+        if ($addressRow['postalCode'] == $postalCodeRow->getPostalCode()) {
+          $addressArray[] = new Address($addressRow['addressId'], $addressRow['street'], $addressRow['streetNr'], $postalCode);
+
+          break;
+        }
+      }
+    }
+    return $addressArray;
+  }
+}
