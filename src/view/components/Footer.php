252a390db5212f7a18ed5d8dc6b29607526ef336
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-10-15 10:47:21 +0200

    Site links designed

--- src/view/components/Footer.php
+++ src/view/components/Footer.php
@@ -4,7 +4,20 @@
 
 <footer class="flex flex-col gap-[4rem]">
   <div class="flex justify-center gap-[2rem]">
-    <div></div>
+    <!-- Site Links -->
+    <div class="min-w-[250px] flex flex-col gap-[1.5rem]">
+      <h4 class="text-[1rem] font-bold leading-tight">Useful Links</h4>
+      <div class="flex flex-col gap-[.75rem]">
+        <a href="/dwp" class="w-fit text-[.875rem] text-textNormal leading-snug ease-in-out duration-[.15s] hover:text-textLight">Home</a>
+        <a href="/dwp/movies" class="w-fit text-[.875rem] text-textNormal leading-snug ease-in-out duration-[.15s] hover:text-textLight">All Movies</a>
+        <a href="/dwp/upcoming" class="w-fit text-[.875rem] text-textNormal leading-snug ease-in-out duration-[.15s] hover:text-textLight">Upcoming Movies</a>
+        <!-- TODO: Scroll down to ticket section -->
+        <a href="" class="w-fit text-[.875rem] text-textNormal leading-snug ease-in-out duration-[.15s] hover:text-textLight">Tickets</a>
+        <!-- TODO: Scroll down to contact section it is in the footer -->
+        <a href="" class="w-fit text-[.875rem] text-textNormal leading-snug ease-in-out duration-[.15s] hover:text-textLight">Contact</a>
+      </div>
+    </div>
+    <!-- Company Info -->
     <div></div>
     <div></div>
   </div>
