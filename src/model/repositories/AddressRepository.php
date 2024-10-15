f4dffbacf9c82ee1635ec79b68e45d34698450b6
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-10-15 09:43:30 +0200

    Address repository try catch block implementation

--- src/model/repositories/AddressRepository.php
+++ src/model/repositories/AddressRepository.php
@@ -12,28 +12,37 @@ class AddressRepository {
     $db = $this->getdb();
     /* Get postal codes */
     $postalCodeQuery = $db->prepare("SELECT * FROM PostalCode");
-    $postalCodeQuery->execute();
-    $postalCodeResult = $postalCodeQuery->fetchAll(PDO::FETCH_ASSOC);
-    $postalCodeArray = [];
-
     /* Get addresses */
     $addressQuery = $db->prepare("SELECT *  FROM `Address`");
-    $addressQuery->execute();
-    $addressResult = $addressQuery->fetchAll(PDO::FETCH_ASSOC);
-    $addressArray = [];
+    try {
+      $postalCodeQuery->execute();
+      $postalCodeResult = $postalCodeQuery->fetchAll(PDO::FETCH_ASSOC);
+      $addressQuery->execute();
+      $addressResult = $addressQuery->fetchAll(PDO::FETCH_ASSOC);
 
-    foreach($postalCodeResult as $row) {
-      $postalCodeArray[] = new PostalCode($row['postalCode'], $row['city']);
-    }
-
-    foreach($addressResult as $addressRow) {
-      /* Find the postal code object that matches the postal code of the address */
-      foreach($postalCodeArray as $postalCodeRow) {
-        if ($addressRow['postalCode'] == $postalCodeRow->getPostalCode()) {
-          $addressArray[] = new Address($addressRow['addressId'], $addressRow['street'], $addressRow['streetNr'], $postalCodeRow);
-        }
+      if (empty($postalCodeResult) || empty($addressResult)) {
+        return null;
       }
+      else {
+        $postalCodeArray = [];
+        $addressArray = [];
+
+        foreach($postalCodeResult as $row) {
+          $postalCodeArray[] = new PostalCode($row['postalCode'], $row['city']);
+        }
+    
+        foreach($addressResult as $addressRow) {
+          /* Find the postal code object that matches the postal code of the address */
+          foreach($postalCodeArray as $postalCodeRow) {
+            if ($addressRow['postalCode'] == $postalCodeRow->getPostalCode()) {
+              $addressArray[] = new Address($addressRow['addressId'], $addressRow['street'], $addressRow['streetNr'], $postalCodeRow);
+            }
+          }
+        }
+        return $addressArray;
+      }
+    } catch (PDOException $e) {
+      return null;
     }
-    return $addressArray;
   }
 }
