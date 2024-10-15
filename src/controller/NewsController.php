7a5957ef64c66c100d06c0e9994cf1840797b809
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-10-15 12:09:30 +0200

    separate get methods ( wasnt a good idea:( )

--- src/controller/NewsController.php
+++ src/controller/NewsController.php
@@ -8,23 +8,12 @@ class NewsController {
         $this->newsRepository = new NewsRepository();
     }
 
-    /**
-     * Retrieves all news items from the database.
-     * 
-     * If an optional id parameter is given, this method will only return the news item with that id.
-     * 
-     * @param int|null $id The id of the news item to retrieve. If null, all news items are retrieved.
-     * 
-     * @return array An array of News objects. If $id is given, the array will contain only one News object.
-     * 
-     * @throws PDOException If the database query fails
-     */
-    public function getNews($id = null) {
-        if ($id) {
-            return $this->newsRepository->get($id);
-        } else {
-            return $this->newsRepository->get();
-        }
+    public function getNews($id = null): array {
+        return $this->newsRepository->getAll();
+    }
+
+    public function getNewsById($id): News {
+        return $this->newsRepository->get($id);
     }
 }
 ?>
--- src/model/repositories/NewsRepository.php
+++ src/model/repositories/NewsRepository.php
@@ -7,27 +7,31 @@ class NewsRepository {
         return DatabaseConnection::getInstance();
     }
 
-    public function get($id = null) {
+    public function get($id): News {
+        $db = $this->getdb();
+        try {
+            $query = $db->prepare("SELECT * FROM News n WHERE n.newsId = :id");
+            $query->execute(array(":id" => $id));
+            $result = $query->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo 'Failed to fetch news: '. $e;
+        }
+        return new News($result['newsId'], $result['imageURL'], $result['header'], $result['content']);
+    }
+
+    public function getAll(): array {
         $db = $this->getdb();
         $retarray = [];
-        $retVal = null;
         try {
-            if ($id){
-                $query = $db->prepare("SELECT * FROM News n WHERE n.newsId = :id");
-                $query->execute(array(":id" => $id));
-                $result = $query->fetch(PDO::FETCH_ASSOC);
-                $retVal = new News($result['newsId'], $result['imageURL'], $result['header'], $result['content']);
-            } else {
-                $query = $db->prepare("SELECT * FROM News");
-                $query->execute();
-                $result = $query->fetchAll(PDO::FETCH_ASSOC);
-                foreach($result as $row) {
-                    $retarray[] = new News($row['newsId'], $row['imageURL'], $row['header'], $row['content']);
-                }
-                return $retarray;
+            $query = $db->prepare("SELECT * FROM News");
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+            foreach($result as $row) {
+                $retarray[] = new News($row['newsId'], $row['imageURL'], $row['header'], $row['content']);
             }
         } catch (PDOException $e) {
             echo 'Failed to fetch news: '. $e;
         }
+        return $retarray;
     }
 }
