7569fda5151fb6bcced041b025e8de51111a13c6
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-10-15 10:31:48 +0200

    Footer base structure and layout

---
+++ src/view/components/Footer.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+
+<footer>
+  <div class="flex justify-center gap-[2rem]">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+  <div class="font-light">&copy; cinema all rights reserved</div>
+</footer>
