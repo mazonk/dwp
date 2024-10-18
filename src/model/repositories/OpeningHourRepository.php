3a73be2e394601d0dc148eb7ad42eba5ce1e6b63
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-10-18 11:20:26 +0200

    Service logic separated from repositroy

--- src/model/repositories/OpeningHourRepository.php
+++ src/model/repositories/OpeningHourRepository.php
@@ -1,7 +1,4 @@
 <?php
-include "src/model/entity/OpeningHour.php";
-include_once "src/controller/VenueController.php";
-
 class OpeningHourRepository {
   private function getdb() {
     require_once 'src/model/database/dbcon/DatabaseConnection.php';
@@ -16,39 +13,7 @@ class OpeningHourRepository {
 
     try {
       $query->execute();
-      $result = $query->fetchAll(PDO::FETCH_ASSOC);
-      $retArray = [];
-
-      if ($result) {
-        $venueController = new VenueController();
-        $venue = $venueController->getVenue($venueId);
-
-        foreach($result as $row) {
-          if ($row['isCurrent'] == 1) {
-            // Map string from DB to Day enum
-            $day = match ($row['day']) {
-              'Monday' => Day::Monday,
-              'Tuesday' => Day::Tuesday,
-              'Wednesday' => Day::Wednesday,
-              'Thursday' => Day::Thursday,
-              'Friday' => Day::Friday,
-              'Saturday' => Day::Saturday,
-              'Sunday' => Day::Sunday,
-              default => throw new InvalidArgumentException("Invalid day: " . $row['day'])
-            };
-
-             // Convert strings to DateTime objects
-            $openingTime = new DateTime($row['openingTime']);
-            $closingTime = new DateTime($row['closingTime']);
-
-            $retArray[] = new OpeningHour($row['openingHourId'], $day, $openingTime, $closingTime, $row['isCurrent'], $venue);
-          }
-        }
-        return $retArray;
-      }
-      else {
-        return null;
-      }
+      return $result = $query->fetchAll(PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
       echo "Error: " . $e->getMessage();
       return null;
