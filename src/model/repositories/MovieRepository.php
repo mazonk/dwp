eaf7a5644be6e596e8871f188cadebf1cfb42d0a
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-10-24 17:49:24 +0200

    refactor MovieRepo

--- src/model/repositories/MovieRepository.php
+++ src/model/repositories/MovieRepository.php
@@ -10,41 +10,50 @@ class MovieRepository {
     public function getAllMovies(): array {
         $db = $this->getdb();
         $query = $db->prepare("SELECT * FROM Movie");
-        $query->execute();
-        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        try {
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+            if (empty($result)) {
+                throw new Exception("No movies found");
+            }
+        } catch (PDOException $e) {
+            throw new Exception("Unable to fetch movies: ". $e->getMessage());
+        }
+
         return $result;
     }
-    public function getActorsByMovieId(int $movieId) {
+
+    public function getActorsByMovieId(int $movieId): array {
         $db = $this->getdb();
         $query = $db->prepare("SELECT a.* FROM Actor as a JOIN MovieActor as ma ON a.actorId = ma.actorId WHERE ma.movieId = ?");
         try {
             $query->execute([$movieId]);
             $result = $query->fetchAll(PDO::FETCH_ASSOC);
             if (empty($result)) {
-                return [];
-            } else {
-                return $result;
+                throw new Exception("No actors found for this movie");
             }
         }
         catch (PDOException $e) {
-            echo "Couldn't fetch actors: " . $e->getMessage();
+            throw new Exception("Unable to fetch actors" . $e->getMessage());
         }
+
+        return $result;
     }
 
-    public function getDirectorsByMovieId(int $movieId) {
+    public function getDirectorsByMovieId(int $movieId): array {
         $db = $this->getdb();
         $query = $db->prepare("SELECT d.* FROM Director as d JOIN MovieDirector as md ON d.directorId = md.directorId WHERE md.movieId = ?");
         try {
             $query->execute([$movieId]);
             $result = $query->fetchAll(PDO::FETCH_ASSOC);
             if (empty($result)) {
-                return [];
-            } else {
-                return $result;
+                throw new Exception("No directors found for this movie");
             }
         } catch (PDOException $e) {
-            echo "Couldn't fetch directors: ". $e->getMessage();
+            throw new Exception("Unable to fetch directors: ". $e->getMessage());
         }
+
+        return $result;
     }
 
     public function getMovieById(int $movieId) {
@@ -53,10 +62,14 @@ class MovieRepository {
         try{
             $query->execute([$movieId]);
             $result = $query->fetch(PDO::FETCH_ASSOC);
-            return $result;
+            if (empty($result)) {
+                throw new Exception("No movie found with id: ". $movieId);
+            }
         }
         catch (PDOException $e) {
             echo "Couldn't fetch movie: " . $e->getMessage();
         }
+        
+        return $result;
     }
 }
