b66b23af274cff7ebf5ab918600363c21fc534f2
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-10-25 10:53:31 +0200

    reworked moviecard form -> a tag for routing

--- src/view/components/MovieCard.php
+++ src/view/components/MovieCard.php
@@ -8,15 +8,10 @@ class MovieCard {
         ?>
           <body>
         <div class="w-[12.5rem] text-center m-[0.625rem] movie-card">
-         <form action="<?php echo $_SESSION['baseRoute'] ?>movies/<?php echo $movie->getMovieId(); ?>" method="GET">
-                <!-- Hidden input to pass the movie ID -->
-                <input type="hidden" name="id" value="<?php echo $movie->getMovieId(); ?>">
-                <!-- Submit button with the poster image -->
-                <button type="submit">
-                  <img class="w-full h-[18.75rem] rounded-[0.625rem] m-[0.625rem] bg-center bg-cover" 
-                  src="src/assets/<?php echo $movie->getPosterURL(); ?>" alt="Movie Poster">
-                </button>
-              </form>
+          <a href="<?php echo $_SESSION['baseRoute'] ?>movies/<?php echo $movie->getMovieId(); ?>">
+            <img class="w-full h-[18.75rem] rounded-[0.625rem] m-[0.625rem] bg-center bg-cover" 
+            src="src/assets/<?php echo $movie->getPosterURL(); ?>" alt="Movie Poster">
+          </a>
         <div class="text-[1.2rem] text-white"><?php echo htmlspecialchars($movie->getTitle()); ?></div>
 
           <?php if ($showReleaseDate):?>
