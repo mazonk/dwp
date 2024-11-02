16e10507d458e04cfa789f73ec896f2425bc05b2
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-11-02 17:13:25 +0100

    edit company info in controller

--- src/controller/CompanyInfoController.php
+++ src/controller/CompanyInfoController.php
@@ -15,4 +15,19 @@ class CompanyInfoController {
         }
         return $companyInfo;
     }
+
+    public function editCompanyInfo(int $companyInfoId, array $newCompanyInfoData): array|CompanyInfo {
+        // Validate input data
+        $validationErrors = $this->validateInputs($newCompanyInfoData);
+        if (!empty($validationErrors)) {
+            return ['errorMessage' => implode(', ', $validationErrors)];
+        }
+    
+        // Proceed with the service call if validation passes
+        $companyInfo = $this->companyInfoService->editCompanyInfo($companyInfoId, $newCompanyInfoData);
+        if (is_array($companyInfo) && isset($companyInfo['error']) && $companyInfo['error']) {
+            return ['errorMessage'=> $companyInfo['message']];
+        }
+        return $companyInfo;
+    }
 }
