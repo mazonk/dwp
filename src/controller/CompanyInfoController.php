7be4902259cfc96b8fa8f23fab760854cfcac9c2
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-11-02 15:44:47 +0100

    Create CompanyInfoController.php

---
+++ src/controller/CompanyInfoController.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'src/model/services/CompanyInfoService.php';
+class CompanyInfoController {
+    private CompanyInfoService $companyInfoService;
+
+    public function __construct() {
+        $this->companyInfoService = new CompanyInfoService();
+    }
+
+    /* Get company info */
+    public function getCompanyInfo(): array|CompanyInfo {
+        $companyInfo = $this->companyInfoService->getCompanyInfo();
+        if (isset($companyInfo['error']) && $companyInfo['error']) {
+            return ['errorMessage'=> $companyInfo['message']];
+        }
+        return $companyInfo;
+    }
+}
