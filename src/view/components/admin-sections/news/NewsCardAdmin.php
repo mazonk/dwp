332573f40365921c49905b25e6566360253eb1e2
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-11-06 16:01:23 +0100

    Action buttons added

--- src/view/components/admin-sections/news/NewsCardAdmin.php
+++ src/view/components/admin-sections/news/NewsCardAdmin.php
@@ -4,7 +4,7 @@ class NewsCardAdmin {
         // Shorten the content to max 140 characters
         $shortenedContent = substr($content, 0, 140) . (strlen($content) > 140 ? '... ' : ''); 
         ?>
-        <div class="max-w-[400px] bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+        <div class="max-w-[400px] bg-bgSemiDark rounded-lg shadow-lg overflow-hidden">
           <!-- Clickable link wrapping the image -->
           <a href="<?php echo $_SESSION['baseRoute'] ?>news/<?php echo htmlspecialchars($id); ?>">
             <img class="h-[275px] max-h-[275px] object-cover object-center" src="src/assets/<?php echo htmlspecialchars($imageURL); ?>" alt="Movie Poster">
@@ -13,10 +13,12 @@ class NewsCardAdmin {
           <div class="p-4">
             <a href="<?php echo $_SESSION['baseRoute'] ?>news/<?php echo htmlspecialchars($id)?>" class="text-xl font-semibold text-white mb-2"><?php echo htmlspecialchars($header); ?></a>
 
-            <p class="text-gray-400 text-sm leading-6">
+            <p class="pb-[1rem] text-gray-400 text-sm leading-6">
               <?php echo htmlspecialchars($shortenedContent); ?>
-              <a href="<?php echo $_SESSION['baseRoute'] ?>news/<?php echo htmlspecialchars($id); ?>" class="text-blue-500 hover:underline">Read more</a>
             </p>
+
+            <button id="editCompanyInfoButton" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Edit</button>
+            <button id="deleteNewsButton" class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600">Delete</button>
           </div>
         </div>
         <?php
