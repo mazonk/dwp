8e7901d34f1cc1a7a6d7f02a53619f2aed13a328
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-11-06 15:37:39 +0100

    newsCRUD placement

--- src/view/components/admin-sections/contentManagement.php
+++ src/view/components/admin-sections/contentManagement.php
@@ -1,9 +1,12 @@
-<?php parse_str(file_get_contents("php://input"), $_PUT); ?>
+<?php parse_str(file_get_contents("php://input"), $_PUT);
+include_once "src/controller/NewsController.php";
+
+include_once "src/view/components/NewsCard.php";
+?>
 <section>
     <h2 class="text-3xl font-semibold mb-4 text-center">Content Management</h2>
     <div class="flex flex-col">
-        <hr class="mb-4 w-full mx-auto border-gray-300">
-        <div class="flex flex-col space-y-8">
-        </div>
+        <hr class="w-full mx-auto border-gray-300">
+        <?php include_once("src/view/components/admin-sections/newsCRUD.php"); ?>
     </div>
 </section>
