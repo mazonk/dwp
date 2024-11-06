31482c6f23679f565eede0f24f5f6703000d22d5
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-11-06 12:08:03 +0100

    About page (#30)
    * added nav link to about page
    
    * add route for venueselectdropdown
    
    * display company info (name, logo, desc.)
    
    * "Our venues" section
    
    * getSelectedVenue method in controller
    
    * select a venue from the venue list
    
    * opening hours and address info
    
    * show contact info
    
    * redesigned venue-info card

--- routes.php
+++ routes.php
@@ -71,6 +71,7 @@ get($baseRoute.'upcoming', 'src/view/pages/UpcomingMoviesPage.php');
 post($baseRoute.'movies', 'src/view/pages/AllMoviesPage.php'); // used at toggle dropdown
 post($baseRoute.'upcoming', 'src/view/pages/AllMoviesPage.php'); // used at toggle dropdown
 post($baseRoute.'home', 'src/view/pages/LandingPage.php'); // used at toggle dropdown
+post($baseRoute.'about', 'src/view/pages/AboutPage.php'); // used at toggle dropdown
 
 // Post route for register
 post($baseRoute.'register', function() {
