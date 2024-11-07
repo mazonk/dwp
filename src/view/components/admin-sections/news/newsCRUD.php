598ca558fbfe2c8f15763fcfafc0b37b1e0c050d
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-11-07 11:06:12 +0100

    Error display, modal closing update

--- src/view/components/admin-sections/news/newsCRUD.php
+++ src/view/components/admin-sections/news/newsCRUD.php
@@ -40,6 +40,7 @@ include_once "src/view/components/admin-sections/news/NewsCardAdmin.php";
                     <div class="mb-4">
                         <label for="header" class="block text-sm font-medium text-text-textLight">Header</label>
                         <input type="text" id="header" name="header" class="mt-1 block w-full p-2 bg-bgDark border border-borderDark rounded-md outline-none focus:border-textNormal duration-[.2s] ease-in-out" required>
+                        <p id="error-header"  class="mt-1 text-red-500 hidden text-xs mb-[.25rem]"></p>
                     </div>
                     <!-- <div class="mb-4">
                         <label for="imageURL" class="block text-sm font-medium text-text-textLight">Image</label>
@@ -49,6 +50,7 @@ include_once "src/view/components/admin-sections/news/NewsCardAdmin.php";
                     <div class="mb-4">
                         <label for="content" class="block text-sm font-medium text-text-textLight">Content</label>
                         <textarea id="content" name="content" rows="4" class="mt-1 block w-full p-2 bg-bgDark border border-borderDark rounded-md outline-none focus:border-textNormal duration-[.2s] ease-in-out" required></textarea>
+                        <p id="error-content" class="mt-1 text-red-500 hidden text-xs mb-[.25rem]"></p>
                     </div>
                     <div class="flex justify-end">
                         <button type="submit" id="saveAddNewsButton" class="bg-primary text-textDark py-2 px-4 rounded border border-transparent hover:bg-primaryHover duration-[.2s] ease-in-out">Add</button>
@@ -66,6 +68,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const addNewsModal = document.getElementById('addNewsModal');
     const addNewsForm = document.getElementById('addNewsForm');
     const addNewsButton = document.getElementById('addNewsButton');
+    const errorMessageHeader = document.getElementById('error-header');
+    const errorMessageContent = document.getElementById('error-content');
     /* const imageInput = document.getElementById('imageURL'); */
 
     // Display the modal
@@ -73,10 +77,18 @@ document.addEventListener('DOMContentLoaded', () => {
         addNewsModal.classList.remove('hidden');
     });
 
-    // Hide the modal
+    // Close the modal
     document.getElementById('cancelAddNewsButton').addEventListener('click', () => {
         addNewsModal.classList.add('hidden');
+        clearValues();
     });
+
+    // Clear error messages and input values
+    function clearValues() {
+        errorMessageHeader.classList.add('hidden');
+        errorMessageContent.classList.add('hidden');
+        addNewsForm.reset();
+    }
 
     addNewsForm.addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent default form submission
@@ -107,8 +119,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (response.success) {
                     alert('Success! News added successfully.');
                     window.location.reload();
+                    clearValues();
                 } else {
-                    console.error('Error:', response.errorMessage);
+                    // Display error messages
+                    if (response.errors['header']) {
+                        errorMessageHeader.textContent = response.errors['header'];
+                        errorMessageHeader.classList.remove('hidden');
+                    }
+                    if (response.errors['content']) {
+                        errorMessageContent.textContent = response.errors['content'];
+                        errorMessageContent.classList.remove('hidden');
+                    }
+                    console.error('Error:', response.errors);
                 }
             }
         };
