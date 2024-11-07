451c955d5184ecf8e2bbc2a24068c95b2012b28b
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-11-07 11:17:06 +0100

    Removing a tag link to news page

--- src/view/components/admin-sections/news/NewsCardAdmin.php
+++ src/view/components/admin-sections/news/NewsCardAdmin.php
@@ -1,19 +1,17 @@
 <?php
 class NewsCardAdmin {
-    public static function render($id, $header, $imageURL, $content) {
+    public static function render($header, $imageURL, $content) {
         // Shorten the content to max 140 characters
         $shortenedContent = substr($content, 0, 140) . (strlen($content) > 140 ? '... ' : ''); 
         ?>
         <div class="w-[400px] bg-bgSemiDark rounded-lg shadow-lg overflow-hidden">
           <!-- Clickable link wrapping the image -->
-          <a href="<?php echo $_SESSION['baseRoute'] ?>news/<?php echo htmlspecialchars($id); ?>">
-            <img class="h-[275px] max-h-[275px] object-cover object-center" src="src/assets/<?php echo htmlspecialchars($imageURL); ?>" alt="Movie Poster">
-          </a>
+          <img class="h-[275px] max-h-[275px] object-cover object-center" src="src/assets/<?php echo htmlspecialchars($imageURL); ?>" alt="Movie Poster">
           <!-- Content -->
           <div class="p-4">
-            <a href="<?php echo $_SESSION['baseRoute'] ?>news/<?php echo htmlspecialchars($id)?>" class="text-xl font-semibold text-white mb-2"><?php echo htmlspecialchars($header); ?></a>
+            <p class="text-xl font-semibold text-white mb-1"><?php echo htmlspecialchars($header); ?></p>
 
-            <p class="pb-[1rem] text-gray-400 text-sm leading-6">
+            <p class="mb-[1rem] text-gray-400 text-sm leading-6">
               <?php echo htmlspecialchars($shortenedContent); ?>
             </p>
 
