c1082031eed911782c6f26ac5fa67bff65afb7dd
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-11-08 10:09:21 +0100

    Id added to the card as hidden element, onclick function added to edit and delete buttons

--- src/view/components/admin-sections/news/NewsCardAdmin.php
+++ src/view/components/admin-sections/news/NewsCardAdmin.php
@@ -1,6 +1,6 @@
 <?php
 class NewsCardAdmin {
-    public static function render($header, $imageURL, $content) {
+    public static function render($newsId, $header, $imageURL, $content) {
         // Shorten the content to max 140 characters
         $shortenedContent = substr($content, 0, 140) . (strlen($content) > 140 ? '... ' : ''); 
         ?>
@@ -9,6 +9,7 @@ class NewsCardAdmin {
           <img class="h-[275px] max-h-[275px] object-cover object-center" src="src/assets/<?php echo htmlspecialchars($imageURL); ?>" alt="Movie Poster">
           <!-- Content -->
           <div class="p-4">
+            <p class="text-xs text-gray-400 hidden"><?php echo htmlspecialchars($newsId); ?></p>
             <p class="text-xl font-semibold text-white mb-1"><?php echo htmlspecialchars($header); ?></p>
 
             <p class="mb-[1rem] text-gray-400 text-sm leading-6">
@@ -16,8 +17,8 @@ class NewsCardAdmin {
             </p>
 
             <div class="flex gap-[.5rem]">
-              <button id="editNewsButton" class="text-white py-2 px-4 border-[1px] border-white rounded hover:bg-borderDark duration-[.2s] ease-in-out">Edit</button>
-              <button id="deleteNewsButton" class="bg-red-500 text-white py-2 px-4 border-[1px] border-transparent rounded hover:bg-red-600 duration-[.2s] ease-in-out">Delete</button>
+              <button id="editNewsButton" onclick="openEditModal(<?= htmlspecialchars($newsId); ?>)" class="text-white py-2 px-4 border-[1px] border-white rounded hover:bg-borderDark duration-[.2s] ease-in-out">Edit</button>
+              <button id="deleteNewsButton" onclick="openDeleteModalopenDeleteModal(<?= htmlspecialchars($newsId); ?>)" class="bg-red-500 text-white py-2 px-4 border-[1px] border-transparent rounded hover:bg-red-600 duration-[.2s] ease-in-out">Delete</button>
             </div>
           </div>
         </div>
