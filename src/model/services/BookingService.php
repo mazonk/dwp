51458e6733c62653be1558d0fb27b45f1ceac87c
Katarína Brezániová <10407558@ucn.dk>
2024-11-11 12:08:29 +0100

    BookingService getBookingById
    > adding return type
    > deleted redundant function

--- src/model/services/BookingService.php
+++ src/model/services/BookingService.php
@@ -1,26 +1,25 @@
 <?php
 include_once "src/model/entity/Reservation.php";
+include_once "src/model/services/UserService.php";
 include_once "src/model/repositories/BookingRepository.php";
 
 class BookingService {
     private BookingRepository $bookingRepository;
+    private UserService $userService;
     public function __construct() {
         $this->bookingRepository = new BookingRepository();
+        $this->userService = new UserService();
     }
 
-    public function getBookingById(int $bookingId): array {
+    public function getBookingById(int $bookingId): Reservation |array  {
         try {
             $result = $this->bookingRepository->getBookingById($bookingId);
-            return $result;
-        } catch (Exception $e) {
-            return ['error' => true, 'message' => $e->getMessage()];
-        }
-    }
+            $user = $this->userService->getUserById($result['userId']);
+            if (is_array($user) && isset($user['error']) && $user['error']) {
+                return $user;
+            }
 
-    public function getAllSeatsForBooking(int $bookingId): array {
-        try {
-            $result = $this->bookingRepository->getAllSeatsForBooking($bookingId);
-            return $result;
+            return new Reservation($result['bookingId'], $user, Status::from($result['status']));
         } catch (Exception $e) {
             return ['error' => true, 'message' => $e->getMessage()];
         }
