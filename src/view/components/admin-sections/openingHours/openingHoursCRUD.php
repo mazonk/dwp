e2c4716b59026d18b816018ae4b1057856ffdae3
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-11-12 11:22:05 +0100

    Displaying opening hour cards

--- src/view/components/admin-sections/openingHours/openingHoursCRUD.php
+++ src/view/components/admin-sections/openingHours/openingHoursCRUD.php
@@ -1,4 +1,5 @@
 <?php
+include_once "src/view/components/admin-sections/openingHours/OpeningHoursCard.php";
 ?>
 
 <div>
@@ -23,45 +24,30 @@
 								'id' => $venue->getVenueId(),
 								'name' => $venue->getName(),
 						]);
-						echo "<button class='venueCard bg-bgSemiDark p-6 border-[1px] border-borderDark rounded-lg cursor-pointer duration-[.2s] ease-in-out hover:scale-[1.025] hover:border-borderLight' 
-										data-venue='" . htmlspecialchars($venueData) . "'>";
-						echo "<p class='text-center font-medium text-white'>" . htmlspecialchars($venue->getName()) . "</p>";
-						echo "</button>";
+						echo "
+						<button class='venueCard bg-bgSemiDark p-6 border-[1px] border-borderDark rounded-lg cursor-pointer duration-[.2s] ease-in-out hover:scale-[1.025] hover:border-borderLight' data-venue='" . htmlspecialchars($venueData) . "'>
+							<p class='text-center font-medium text-white'>" . htmlspecialchars($venue->getName()) . "</p>
+						</button>
+						";
 				}
 			}
 			?>
 	</div>
 
+	<!-- Display opening hours for a selected venue in cards -->
 	<?php
 	require_once "src/controller/OpeningHourController.php";
 	$openingHourController = new OpeningHourController();
 	$openingHours = $openingHourController->getOpeningHoursById(1);
 	?>
-	<!-- Display all opening hours as cards -->
 	<div class="grid grid-cols-5 gap-4">
 		<?php
-		foreach ($openingHours as $openingHour) {
-			echo "
-			<div class='bg-bgSemiDark border-[1px] border-borderDark rounded p-4'>
-				<div class='flex justify-between items-center'>
-					<h4 class='text-[1.25rem] font-semibold'>" . htmlspecialchars($openingHour->getDay()) . "</h4>
-					<div>";
-					if ($openingHour->getIsCurrent()) {
-						echo "<p class='py-[.125rem] px-2 text-[.75rem] font-medium text-primary border-[1px] border-primary rounded-full'>Current</p>";
-					}
-					echo "</div>
-				</div>
-				<p>" . htmlspecialchars($openingHour->getOpeningTime()->format('H:i')) . " - " . htmlspecialchars($openingHour->getClosingTime()->format('H:i')) . "</p>
-				<div class='flex justify-start mt-4 gap-[.5rem]'>
-					<button class='py-1 px-2 text-primary border-[1px] border-primary rounded hover:text-primaryHover hover:border-primaryHover duration-[.2s] ease-in-out'>
-							Edit
-					</button>
-					<button class='bg-red-500 text-textDark py-1 px-2 border-[1px] border-red-500 rounded hover:bg-red-600 hover:border-red-600'>
-							Delete
-					</button>
-				</div>
-			</div>
-			";
+		if (isset($openingHours['errorMessage'])) {
+			echo $openingHours['errorMessage'];
+		} else {
+			foreach ($openingHours as $openingHour) {
+				OpeningHoursCard::render($openingHour);
+			}
 		}
 		?>
 	</div>
