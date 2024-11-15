b93b798792d38840691ba2a9787e3fc4131351c5
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-11-15 18:26:18 +0100

    Add opening hour function added

--- src/model/repositories/OpeningHourRepository.php
+++ src/model/repositories/OpeningHourRepository.php
@@ -1,14 +1,13 @@
 <?php
 class OpeningHourRepository {
-  private function getdb() {
-    require_once 'src/model/database/dbcon/DatabaseConnection.php';
-    return DatabaseConnection::getInstance(); // singleton
+  private PDO $db;
+
+  public function __construct($dbCon) {
+    $this->db = $dbCon;
   }
 
-  /* Get opening hours by venueId */
   public function getOpeningHoursById(int $venueId): array {
-    $db = $this->getdb();
-    $query = $db->prepare("SELECT o.*
+    $query = $this->db->prepare("SELECT o.*
             FROM OpeningHour o
             JOIN VenueOpeningHour vo ON o.openingHourId = vo.openingHourId
             WHERE vo.venueId = :venueId");
@@ -21,6 +20,33 @@ class OpeningHourRepository {
       return $result;
     } catch (PDOException $e) {
       throw new PDOException("Unable to fetch opening hours");
+    }
+  }
+  
+  public function addOpeningHour(array $openingHourData): int {
+    $query = $this->db->prepare("INSERT INTO OpeningHour (day, openingTime, closingTime, isCurrent) VALUES (:day, :openingTime, :closingTime, :isCurrent)");
+    try {
+      $query->execute([
+        'day' => $openingHourData['day'],
+        'openingTime' => $openingHourData['openingTime'],
+        'closingTime' => $openingHourData['closingTime'],
+        'isCurrent' => $openingHourData['isCurrent']
+      ]);
+      return $this->db->lastInsertId();
+    } catch (PDOException $e) {
+      throw new PDOException('Failed to add opening hour');
+    }
+  }
+
+  public function addOpeningHourToVenue(int $openingHourId, int $venueId): void {
+    $query = $this->db->prepare("INSERT INTO VenueOpeningHour (venueId, openingHourId) VALUES (:venueId, :openingHourId)");
+    try {
+      $query->execute([
+        'venueId' => $venueId,
+        'openingHourId' => $openingHourId
+      ]);
+    } catch (PDOException $e) {
+      throw new PDOException('Failed to add opening hour to venue');
     }
   }
 }
