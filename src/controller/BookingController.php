b82a7ad036d545ba4b460c5207504cb26cc8a1a0
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-11-17 19:56:15 +0100

    BookingController->getBookingByUserId()

--- src/controller/BookingController.php
+++ src/controller/BookingController.php
@@ -2,8 +2,18 @@
 include_once "src/model/entity/Booking.php";
 
 class BookingController {
+    private BookingService $bookingService;
 
     public function __construct() {
+        $this->bookingService = new BookingService();
+    }
+
+    public function getBookingByUserId(int $userId): array {
+        $bookings = $this->bookingService->getBookingsByUserId($userId);
+        if (isset($bookings['error']) && $bookings['error']) {
+            return ['errorMessage'=> $bookings['message']];
+        }
+        return $bookings;
     }
 }
 
