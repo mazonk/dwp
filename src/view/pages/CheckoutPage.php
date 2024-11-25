7ef49a66cf50c6bd99bed6c7fc85e2264d89e0ef
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-11-25 17:20:57 +0100

    Temp payement file

--- src/view/pages/CheckoutPage.php
+++ src/view/pages/CheckoutPage.php
@@ -1,2 +1,20 @@
 <?php
 require_once 'session_config.php';
+require_once 'loadEnv.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Checkout</title>
+</head>
+<body>
+  <?php
+  loadEnv();
+
+  
+  ?>
+</body>
+</html>
