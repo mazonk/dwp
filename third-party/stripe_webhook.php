0fb2985d36cde040aa0d548b5b07d1f5b6faa7ad
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-11-30 11:41:32 +0100

    webhook code added

--- third-party/stripe_webhook.php
+++ third-party/stripe_webhook.php
@@ -5,7 +5,56 @@ require_once 'loadEnv.php';
 // Load environment variables for Stripe API keys
 loadEnv();
 $stripe_secret_key = getenv('STRIPE_SK');
+$endpoint_secret = getenv('STRIPE_WH_SECRET');
 
 \Stripe\Stripe::setApiKey($stripe_secret_key);
 
+$sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE']; // Retrieve the Stripe-Signature header
+$payload = @file_get_contents("php://input");
+$event = null;
 
+try {
+  // This is the signature verification step
+  $event = \Stripe\Webhook::constructEvent(
+    $payload, $sig_header, $endpoint_secret
+  );
+} catch(\UnexpectedValueException $e) {
+  // Invalid payload
+  http_response_code(400);
+  exit();
+} catch(\Stripe\Exception\SignatureVerificationException $e) {
+  // Invalid signature
+  http_response_code(400);
+  exit();
+}
+
+// Handle the event
+switch ($event->type) {
+  case: 'checkout.session.completed':
+    $eventData = $event->data->object;
+    // Update payment status to confirmed, and send invoice
+    
+    // TODO: Implement the following functions
+    /* udatePaymentStatus($eventData->id, 'completed');
+    sendInvoice($eventData); */
+    break;
+
+  // List other event types when the payment fails or canceled or expires so it wasnt completed
+
+  
+
+  case: 'payment_intent.payment_failed':
+    $eventData = $event->data->object;
+    // Update payment status to failed
+
+    // TODO: Implement the following functions
+    /* udatePaymentStatus($eventData->id, 'failed'); */
+    break;
+
+  default:
+    // Unexpected event type
+    http_response_code(400);
+    exit();
+}
+
+http_response_code(200);
