c5d4c3fb05cbeeb3e5c5bb3002d0687a88b5c1f8
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-12-12 11:32:18 +0100

    include component

--- src/view/components/admin-sections/bookingsInvoices.php
+++ src/view/components/admin-sections/bookingsInvoices.php
@@ -1,3 +1,4 @@
 <section>
     <h2 class="text-3xl font-semibold mb-4 text-center">Bookings & Invoices</h2>
+    <?php require_once 'src/view/components/admin-sections/movies/moviesWithShowings.php'?>
 </section>
