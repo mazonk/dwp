b2925da14a7125bcdda534eb11e93240ea87bf39
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2024-12-14 11:42:55 +0100

    deleting manually added files

--- src/model/services/captcha/captcha.php
+++
@@ -1,8 +0,0 @@
-<?php
-        include_once "session_config.php";
-        include_once ("./phptextClass.php");  
-        
-        $phptextObj = new phptextClass();       
-        /*phptext function to genrate image with text*/
-        $phptextObj->phpcaptcha('#162453','#fff',120,40,10,25);      
- ?>
