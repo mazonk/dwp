2c5bf9eb5417beafc7b5b14523f212ee67c1157d
Norbert Tolnai <tolnainorbi16@gmail.com>
2024-12-17 22:31:39 +0100

    New file created instead

--- src/view/components/admin-sections/scheduling.php
+++
@@ -1,4 +0,0 @@
-<section>
-    <h2 class="text-3xl font-semibold mb-4 text-center">Scheduling</h2>
-    <?php include_once("src/view/components/admin-sections/showings/showingCRUD.php"); ?>
-</section>
