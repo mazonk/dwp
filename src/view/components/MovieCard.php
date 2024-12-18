c81418f40589419545df065ac630696ddfca37b2
Katarína Brezániová <katkabrezani@gmail.com>
2024-12-18 05:07:16 +0100

    -_-

--- src/view/components/MovieCard.php
+++ src/view/components/MovieCard.php
@@ -8,7 +8,7 @@ class MovieCard {
         <body>
         <div class="w-[12.5rem] text-center m-[0.625rem] movie-card group">
             <!-- Wrap the image and title with a link -->
-            <a href="<?php echo $_SESSION['baseRoute'] ?>movie/<?php echo $movie->getMovieId(); ?>">
+            <a href="<?php echo $_SESSION['baseRoute'] ?>movies/<?php echo $movie->getMovieId(); ?>">
                 <img class="w-full h-[18.75rem] rounded-[0.625rem] m-[0.625rem] bg-center bg-cover transition-transform duration-300 ease-in-out transform group-hover:scale-105" 
                     src="src/assets/<?php echo $movie->getPosterURL(); ?>" alt="Movie Poster">
                 <div class="text-[1.2rem] text-white transition-transform transform duration-300 ease-in-out group-hover:scale-105">
